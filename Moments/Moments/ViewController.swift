7332dbb553f6a1d6b43e6fc1cbe6cebf04e8473c
陈冰 <cb2@meitu.com>
2023-04-03 16:01:49 +0800

    【Feature】空白项目初始化

---
+++ Moments/Moments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moments
+//
+//  Created by ChenBing on 2023/4/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
